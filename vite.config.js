184f042f81783c90a83633acd4d601c63e293672
Zaw Myat Min <zawmyatminn1@gmail.com>
2024-12-22 19:44:02 +0630

    change some code and compile

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base:'/portfolio/',
+  base:'/zawmyatmin/',
   plugins: [
     vue(),
   ]
